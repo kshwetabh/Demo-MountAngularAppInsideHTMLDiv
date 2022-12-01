689389328d24f11171becf1d9025b19556a136f3
Kumar Shwetabh Fnu <kumarshwetabh.fnu@infor.com>
2022-12-01 11:19:29 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  // selector: 'app-root',
+  selector: '#angular-root', //Ext component ID as the mount point.
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'test-app';
+  @ViewChild('selection') input: ElementRef;
+
 }
